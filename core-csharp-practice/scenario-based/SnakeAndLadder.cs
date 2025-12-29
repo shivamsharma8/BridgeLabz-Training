2eb05886008df4508ef4841596219e037e56bf23
shivam sharma <shivam832005sharma@gmail.com>
2025-12-29 23:49:37 +0530

    [Shivam Sharma]: Added Solutions Of Scenario Based Practice Problems

---
+++ core-csharp-practice/scenario-based/SnakeAndLadder.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Scenario_Based
+{
+    internal class SnakeAndLadder
+    {
+        static Random random = new Random();
+
+        // Snake and Ladder 
+        static int[] snakeStart = { 25, 52, 70, 99 };
+        static int[] snakeEnd = { 5, 42, 55, 10 };
+
+        static int[] ladderStart = { 3, 11, 40, 60 };
+        static int[] ladderEnd = { 22, 49, 80, 95 };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SNAKE & LADDER GAME");
+
+            Console.Write("Enter number of players (2-4): ");
+            int playerCount = Convert.ToInt32(Console.ReadLine());
+
+            if (playerCount < 2 || playerCount > 4)
+            {
+                Console.WriteLine("Invalid Players must be between 2 to 4.");
+                return;
+            }
+
+            string[] players = new string[playerCount];
+            int[] pos = new int[playerCount];
+
+            // Taking user names
+            for (int i = 0; i < playerCount; i++)
+            {
+                Console.Write("Enter Player " + (i + 1) + " Name: ");
+                players[i] = Console.ReadLine();
+                pos[i] = 0;
+            }
+
+            Console.WriteLine("\nGame Started Reach EXACT 100 to Win");
+            Console.WriteLine("Press ENTER to roll dice\n");
+
+            bool gameOn = true;
+
+            while (gameOn)
+            {
+                for (int turn = 0; turn < playerCount; turn++)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine(players[turn] + "'s turn");
+                    Console.WriteLine("Press ENTER to roll dice");
+                    Console.ReadLine(); 
+
+                    int dice = RollDice();
+                    Console.WriteLine("Dice Rolled: " + dice);
+
+                    int oldPos = pos[turn];
+                    int newPos = oldPos + dice;
+
+                    if (newPos > 100)
+                    {
+                        Console.WriteLine("Move exceeds 100");
+                        Console.WriteLine("Position remains: " + pos[turn] + "\n");
+                        continue;
+                    }
+
+                    pos[turn] = newPos;
+
+                    pos[turn] = ApplySnakeOrLadder(pos[turn]);
+
+                    Console.WriteLine("Position: " +oldPos + " - " +pos[turn]);
+
+                    if (pos[turn] == 100)
+                    {
+                        Console.WriteLine("\n");
+                        Console.WriteLine(players[turn] + " WINS");
+                        Console.WriteLine("");
+                        gameOn = false;
+                        break;
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("\nGame Over. Thanks for playing");
+        }
+
+        
+        static int RollDice()
+        {
+            return random.Next(1, 7); 
+        }
+
+        static int ApplySnakeOrLadder(int pos)
+        {
+            // Snake 
+            for (int i = 0; i < snakeStart.Length; i++)
+            {
+                if (pos == snakeStart[i])
+                {
+                    Console.WriteLine("Snake Bite Slide to " + snakeEnd[i]);
+                    return snakeEnd[i];
+                }
+            }
+
+            // Ladder 
+            for (int i = 0; i < ladderStart.Length; i++)
+            {
+                if (pos == ladderStart[i])
+                {
+                    Console.WriteLine("Ladder Climb to " + ladderEnd[i]);
+                    return ladderEnd[i];
+                }
+            }
+
+            return pos;
+        }
+    }
+
+}
---
+++ core-csharp-practice/scenario-based/StringEditor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Scenario_Based
+{
+    internal class StringEditor
+    {
+        //Method to add Space after Punctuations in the given String
+        public static string SpaceAdder(string str) {
+
+            string output = "";
+
+            for (int i = 0; i < str.Length; i++) { 
+                char c = str[i];
+                output += c;
+
+                if (c == '.' || c == ',' || c == '!' || c == ';' || c == ':' || c == '?') {
+                    if(i+1 < str.Length &&  str[i+1] == ' ')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        output += ' ';
+                    }
+                }
+
+            }
+
+            return output;
+        }
+        //Method to Capitalize the first leeter of word comming after "?",".","!"
+        public static string Capitalizer(string str)
+        {
+            string output = "";
+
+            for (int i = 0; i < str.Length;i++)
+            {
+                char c = str[i];
+                output += c;
+                if(c == '.' || c == '!' || c == '?') {
+                    if (str[i + 1] >= 97 && str[i + 1] <= 122)
+                    {
+                        i += 1;
+                        c = ( char)(str[i]-32);
+                        output += c;
+                        //str[i + 1] = (char)(str[i + 1] - 32);
+
+                    }
+                    else if (str[i + 1] == ' ' && (str[i + 2] >= 97 && str[i + 2] <= 122))
+                    {
+                        i += 2;
+                        c = (char)(str[i] - 32);
+                        output += c;
+                        //str[i + 2] = (char)(str[i + 2] - 32);
+                    }
+                }
+            }
+
+            return output;
+        }//Method to remove extra spaces (Just like .Trim() function)
+        public static string SpaceTrimmer(string str)
+        {
+            string output = "";
+
+            for(int i = 0;i < str.Length; i++)
+            {
+                char c = str[i];
+
+                if (c == ' ' && (i + 1 < str.Length) && str[i + 1] == ' ') {
+                    continue;
+                }
+                else
+                {
+                    output += c;
+                }
+                   
+            }
+
+            return output;
+        }
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter the String");
+            string str = Console.ReadLine();
+
+            str = SpaceAdder(str);
+            Console.WriteLine("After SpaceAdder: "+str);
+            str = Capitalizer(str);
+            Console.WriteLine("After Capitalizer: " + str);
+            str = SpaceTrimmer(str);
+            Console.WriteLine("After SpaceTrimmer: "+ str);
+            
+            
+        }
+    }
+}
