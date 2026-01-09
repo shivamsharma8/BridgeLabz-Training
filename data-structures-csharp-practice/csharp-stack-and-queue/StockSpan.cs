9dbb66449fab07d63910315047d549dde04ac3db
shivamsharma8 <113785642+shivamsharma8@users.noreply.github.com>
2026-01-09 22:07:23 +0530

    [Shivam-Sharma] : Added Solutions Of Stack-And-Queue-Practice-Problems

---
+++ data-structures-csharp-practice/csharp-stack-and-queue/StockSpan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.DataStructures.StackAndQueue
+{
+    class StockSpan
+    {        public static int[] CalculateSpan(int[] prices)
+        {
+            int totalDays = prices.Length;
+            int[] stockSpan = new int[totalDays];
+           Stack<int> stackInput = new Stack<int>();
+            for (int i = 0; i < totalDays; i++)
+            {
+                while (stackInput.Count > 0 && prices[stackInput.Peek()] <= prices[i])
+                {
+                  stackInput.Pop();
+                }
+              stockSpan[i] = (stackInput.Count == 0) ? (i + 1) : (i - stackInput.Peek());                stackInput.Push(i);
+            }
+            return stockSpan;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] pricesOfStock = { 100, 80, 60, 70, 60, 75, 85 };
+            int[] result = CalculateSpan(pricesOfStock);
+            Console.WriteLine("Stock Prices:");
+            foreach (int price in pricesOfStock)
+            {
+                Console.Write(price + " ");
+            }
+            Console.WriteLine("\nSpans:");
+            foreach (int span in result)
+            {
+                Console.Write(span + " ");
+            }
+        }
+    }
+}
