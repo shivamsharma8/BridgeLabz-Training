eed873bf92b61dbf53cb4c8a514d9901b58c8428
shivamsharma8 <113785642+shivamsharma8@users.noreply.github.com>
2026-01-11 22:35:13 +0530

    [Shivam Sharma] : Added Solutions Of Sorting Practice Problems

---
+++ data-structures-csharp-practice/csharp-sorting-algorithm/SortExamScores.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.DataStructures.SortingAlgorithm
+{
+    internal class SortExamScores
+    {
+        static void Main(string[] args)
+        {
+            int[] examScores = new int[10];
+            for (int i = 0; i < examScores.Length; i++)
+            {
+                examScores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            SelectionSort(examScores);
+
+            Console.WriteLine("After Sorting :");
+            PrintArray(examScores);
+        }
+
+        // Selection Sort 
+        static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+        }
+        static void PrintArray(int[] arr)
+        {
+            foreach (int score in arr)
+            {
+                Console.Write(score + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ data-structures-csharp-practice/csharp-sorting-algorithm/SortStudentAges.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabz_Training.DataStructures.SortingAlgorithm
+{
+    internal class SortStudentAges
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the ages of students.");
+            int[] ages = new int[10];
+            for (int i = 0; i < ages.Length; i++)
+            {
+                ages[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+       
+
+            CountingSort(ages, 10, 18);
+
+            Console.WriteLine("After Sorting :");
+            PrintArray(ages);
+        }
+
+        // Counting Sort 
+        static void CountingSort(int[] arr, int minAge, int maxAge)
+        {
+            int range = maxAge - minAge + 1;
+            int[] count = new int[range];
+            int[] output = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count[arr[i] - minAge]++;
+            }
+
+            for (int i = 1; i < count.Length; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int currentAge = arr[i];
+                int index = count[currentAge - minAge] - 1;
+                output[index] = currentAge;
+                count[currentAge - minAge]--;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = output[i];
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int age in arr)
+            {
+                Console.Write(age + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
